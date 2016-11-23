fe2d4d1ceb83a3ad7b83840a6a05b72e792b7f2e
Martin Sipek <martin.sipek@live.com>
2016-11-23 20:27:39 +0100

    new port variable

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var rest = require('./routes/rest');
 var index = require('./routes/index');
 var app = express();
 
+var PORT = 3000;
+
 function REST() {
     var self = this;
     self.connectMysql();
@@ -60,8 +62,8 @@ REST.prototype.configureExpress = function(connection) {
 };
 
 REST.prototype.startServer = function() {
-    app.listen(3000,function(){
-        console.log("All right ! I am alive at Port 3000. Timestamp: "+ Date.now());
+    app.listen(PORT,function(){
+        console.log("All right ! I am alive at Port "+PORT+". Timestamp: "+ Date.now());
     });
 };
 
